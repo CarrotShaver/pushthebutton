bcc5c3df43cfdda997deab54cfac4e637ef17c5e
Kaleo Norman <kaleonorman@gmail.com>
2018-01-27 19:14:12 -1000

    Add files via upload
    start menu

---
+++ start.py
@@ -0,0 +1,14 @@
+import sys
+import time
+
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint 
+from pyfiglet import figlet_format
+
+cprint(figlet_format('Push the Button', font='starwars'),
+        attrs=['bold'])
+
+time.sleep(3)
+
+input("Press ENTER to START")
