d6ca488bf2bc9d32b5c7edb1f2e06a2381b71d05
CarrotShaver <rossim@hawaii.edu>
2018-01-28 15:32:07 -1000

    added main() and set up PushTheButton.py

--- PushTheButton.py
+++ PushTheButton.py
@@ -1,12 +1,14 @@
+import start
 import level1
 import level2
 import level3
 import level4
 
 def main():
+	start.Main()
 	level1.main()
-	level2.main()
-	level3.main()
+	level2.Main()
+	level3.Main()
 	level4.main()
 
 main()
--- start.py
+++ start.py
@@ -6,9 +6,13 @@ init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
 from termcolor import cprint 
 from pyfiglet import figlet_format
 
-cprint(figlet_format('Push the Button', font='starwars'),
-        attrs=['bold'])
+def Main():
+    cprint(figlet_format('Push the Button', font='starwars'),
+            attrs=['bold'])
 
-time.sleep(3)
+    time.sleep(3)
 
-input("Press ENTER to START")
+    input("Press ENTER to START")
+
+if __name__=="__main__":
+    Main()
