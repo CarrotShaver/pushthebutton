6daa034f2316f6895d4a4bca657cda7eeff59cdd
CarrotShaver <rossim@hawaii.edu>
2018-01-28 15:49:30 -1000

    fixed bugs

--- start.py
+++ start.py
@@ -1,12 +1,13 @@
 import sys
 import time
 
-from colorama import init
-init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
-from termcolor import cprint 
-from pyfiglet import figlet_format
+
 
 def Main():
+    from colorama import init
+    init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
+    from termcolor import cprint
+    from pyfiglet import figlet_format
     cprint(figlet_format('Push the Button', font='starwars'),
             attrs=['bold'])
 
